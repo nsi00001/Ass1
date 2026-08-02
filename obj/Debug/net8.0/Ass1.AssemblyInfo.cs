e3d9ab22f63bcaa0590de5f19e7a8803e5d81b0c
Dennsi Nsi <nsi00001@algonquinlive.com>
2026-08-02 18:53:15 -0400

    1st commit

--- obj/Debug/net8.0/Ass1.AssemblyInfo.cs
+++ obj/Debug/net8.0/Ass1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ass1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+477e2e23c99341900f3331ea76d868b0e81694e5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+92f623ca294758246e694f3fc6bf1b250d42ebd9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ass1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ass1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- obj/Release/net8.0/Ass1.AssemblyInfo.cs
+++ obj/Release/net8.0/Ass1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ass1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+92f623ca294758246e694f3fc6bf1b250d42ebd9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ass1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ass1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
